bde60521b95d492ccbeb3da0a6bf27e84b9a90b3
Kayla Anderson <kaylanicolette@live.com>
2019-08-31 18:08:08 -0500

    Styling tetris

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStage } from '../gameHelpers';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 // Components
 import Stage from './Stage';
 import Display from './Display';
@@ -9,15 +10,19 @@ const Tetris = () => {
 
   return (
     <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
+      <StyledTetrisWrapper>
+        <StyledTetris>
+          <Stage stage={createStage()} />
+          <aside>
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+            <StartButton />
+          </aside>
+        </StyledTetris>
+      </StyledTetrisWrapper>
     </div>
   );
 };
