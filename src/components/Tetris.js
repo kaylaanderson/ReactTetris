dc61dfc5d4ac6fe8d4359fd045667502d5fbeaef
Kayla Anderson <kaylanicolette@live.com>
2019-08-31 20:31:09 -0500

    Setting up useGameStatus hook

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { createStage } from '../gameHelpers';
+import React, { useState} from 'react';
+// Styled Components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+// Custom Hooks
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
+import { useGameStatus } from '../hooks/useGameStatus';
 // Components
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared);
 
   return (
     <div>
       <StyledTetrisWrapper>
         <StyledTetris>
-          <Stage stage={createStage()} />
+          <Stage stage={stage} />
           <aside>
+            {gameOver} 
             <div>
               <Display text="Score" />
               <Display text="Rows" />
